57eaf6b4a5db4a0dade3f601978a301d450d634a
xusupeng <xusupeng@beisen.com>
2025-05-12 08:25:07 +0800

    refactor(embedding-models):优化意图识别代码结构和可读性
    - 重新组织代码结构，优化模块导入和变量命名- 添加注释以解释各个步骤的目的
    - 简化了一些操作，例如直接使用 llmClient() 初始化 LLM
    - 删除了一些冗余的注释和空行

--- langchainStudy/embedding-models/embed_intention_recognition.py
+++ langchainStudy/embedding-models/embed_intention_recognition.py
@@ -1,39 +1,42 @@
 from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.retrievers import EnsembleRetriever
 from langchain_community.document_loaders import TextLoader
+from langchain_community.retrievers import BM25Retriever
 from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_openai import ChatOpenAI
-from langchain_openai import OpenAIEmbeddings
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 
 from llm_basic.llm_client import llmClient, embeddingClient
 
-# 数据导入
+# 数据加载
 loader = TextLoader(".././resource/qa.txt", encoding="UTF-8")
 docs = loader.load()
-# 数据切分
+
+# 文本切分
 text_splitter = RecursiveCharacterTextSplitter()
 documents = text_splitter.split_documents(docs)
-# 创建embedding
+
+# 创建 embedding 并构建向量数据库
 embeddings = embeddingClient()
-# 通过向量数据库存储
-vector = FAISS.from_documents(documents, embeddings)
-# 查询检索
-# 创建 prompt
+vectorstore = FAISS.from_documents(documents, embeddings)  # 更清晰的变量名
+
+# 构建 prompt
 prompt = ChatPromptTemplate.from_template("""仅根据提供的上下文回答以下问题：:
 <context>
 {context}
 </context>
 Question: {input}""")
-# 创建模型
-llm = llmClient()
-# 创建 document 的chain 查询
-document_chain = create_stuff_documents_chain(llm, prompt)
-from langchain.chains import create_retrieval_chain
 
-# 创建搜索chain 返回值为 VectorStoreRetriever
-retriever = vector.as_retriever()
+# 初始化 LLM 和 document chain
+llm = llmClient()
+document_chain = create_stuff_documents_chain(llm, prompt)
+
+# 设置检索器
+retriever = vectorstore.as_retriever()
 retrieval_chain = create_retrieval_chain(retriever, document_chain)
-# 执行请求
+
+# 执行基本检索
 response = retrieval_chain.invoke({"input": "学习任务"})
 print(response["answer"])
